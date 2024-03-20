cc601c96db3423dfbdb231e5ee83980822ef10cb
Ilya Bokin <ilya.bokin@gmail.com>
2024-03-20 23:38:05 +0300

    add localizable and terms and conditions

---
+++ NewsToDay/Modules/Terms & Conditions/View/TermsConditionsView.swift
@@ -0,0 +1,12 @@
+//
+//  TermsConditionsView.swift
+//  NewsToDay
+//
+//  Created by Ilya on 20.03.2024.
+//
+
+import UIKit
+
+class TermsConditionsView: UIView {
+
+}
---
+++ NewsToDay/Modules/Terms & Conditions/ViewController/TermsConditionsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TermsConditionsViewController.swift
+//  NewsToDay
+//
+//  Created by Ilya on 20.03.2024.
+//
+
+import UIKit
+import SnapKit
+
+class TermsConditionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
