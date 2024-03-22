88a1b539c01a9e78d4ec0c74e82269dd528c91a2
KateKashko <107617602+KateKashko@users.noreply.github.com>
2024-03-22 14:25:35 +0300

    Add DetailModel

--- NewsToDay/Modules/Detail/Model/DetailModel.swift
+++ NewsToDay/Modules/Detail/Model/DetailModel.swift
@@ -5,4 +5,12 @@
 //  Created by NikitaKorniuk   on 17.03.2024.
 //
 
-import Foundation
+import UIKit
+
+struct NewsDetails {
+    let image: UIImage
+    let category: String
+    let title: String
+    let author: String
+    let text: String
+}
