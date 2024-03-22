752e89e3547b718d41d011d72b74b6d94ba9bcf9
KateKashko <107617602+KateKashko@users.noreply.github.com>
2024-03-22 14:26:13 +0300

    made some code refactoring

--- NewsToDay/Modules/Detail/View/DetailView.swift
+++ NewsToDay/Modules/Detail/View/DetailView.swift
@@ -5,4 +5,12 @@
 //  Created by NikitaKorniuk   on 17.03.2024.
 //
 
-import Foundation
+import UIKit
+
+extension UIButton {
+    convenience init(systemImageName: String) {
+        self.init(type: .system)
+        setImage(UIImage(systemName: systemImageName), for: .normal)
+        self.tintColor = .white
+    }
+}
